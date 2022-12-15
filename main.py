3fde60b41c67963e4213e0019d3450dfeb03ecae
Jorge <alfred.jorge@gmail.com>
2022-12-15 18:08:37 -0300

    Update the table

--- main.py
+++ main.py
@@ -20,7 +20,31 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # add a header
+    columns_invoices = df.columns
+    columns_invoices = [item.replace("_", " ").title() for item in columns_invoices]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns_invoices[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns_invoices[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns_invoices[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns_invoices[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns_invoices[4], border=1, ln=1)
+
+    # add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        # pdf.cell expect a string
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
 
